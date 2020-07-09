fe859572b0533159573ef29ae981b977d41fb04b
Daniel VonMoser <danny@Daniels-MacBook-Air.local>
2020-07-09 13:55:35 -0400

    Some changes

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,20 @@
 // +++++++++++++++++DATABASE CONNECTION++++++++++++++++++
 
 var mysql = require("mysql");
-var connection;
 
-if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-    connection = mysql.createConnection({
-        host: "localhost",
-        user: "root",
-        password: "hacktheplanet",
-        database: "todoagain_db",
-        port: 8889
-    });
-};
+//if (process.env.JAWSDB_URL) {
+//    connection = mysql.createConnection(process.env.JAWSDB_URL);
+//} else {
+const connection = mysql.createConnection({
+host: "localhost",
+port: 3306,
+user: "root",
+password: "1qazxsw2",
+database: "burgers_db",
+socket: "/tmp/mysql.sock"
+
+});
+// };
 
 connection.connect(function (err) {
     if (err) {
